c258fc1bca310a72e2ae8175fb467e5fc5fa112d
Berkan-87 <sby.notess@gmail.com>
2025-02-23 01:56:36 +0300

    goit-js-hw

---
+++ src/api/pixabay.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_KEY = '49003372-3de9a450de0a99d64583d6c94'; // API anahtarınızı doğru şekilde buraya ekleyin
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function fetchImages(query) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
